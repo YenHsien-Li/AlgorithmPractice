08029a1648106418968657d35d4e75f50464443e
martin_li <martin.li@aoitek.com>
2024-05-06 15:13:56 +0800

    add myAlgo

---
+++ UnitTest/first_test.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+#include "myAlgo.h"
+
+// Define your test cases
+TEST(TestCase1, TestName1) {
+    // Your test assertions here
+    EXPECT_EQ(1, test::func());
+}
---
+++ lib/myAlgo/test.cpp
@@ -0,0 +1,6 @@
+#include "test.h"
+
+int test::func()
+{
+	return 1;
+}
